1660bbf86adeaf34cae52175d23ccc7655b3ad12
Rodolfo <fororus12@gmail.com>
2024-12-12 17:53:09 -0400

    Functions

--- assets/Rates/index.js
+++
@@ -1,2 +0,0 @@
-export * from './convertRates';
-export * from './getRates';
--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,13 @@
 import { mostrarMoneda } from './mostrarMoneda.js';
 import { validateForm } from './form/validateForm.js';
-import { convertRates, getRates } from './Rates';
+import { convertRates } from './Rates/convertRates.js';
+import { getRates } from './Rates/getRates.js';
 
 const selects = document.getElementsByTagName('select');
 const input = document.getElementsByTagName('input');
 const button = document.getElementsByTagName('button');
+
+validateForm(selects);
 
 // FUNCION QUE RECIBE PARAMETROS PARA MOSTRAR MOENDA
 mostrarMoneda("Bitcoin", "10", "Btc a usd");
