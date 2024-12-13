e3ac412df906929225dc72822281a549d564e55e
Rodolfo <fororus12@gmail.com>
2024-12-13 14:20:46 -0400

    date-fns added

--- assets/form/validateForm.js
+++ assets/form/validateForm.js
@@ -8,6 +8,8 @@
 
 export const validateForm = ( element, errorFeedbackElement ) => {
     if(isNaN(Number(element.value))){
-        
+        console.log('No es numero');
     }
+
+    errorFeedbackElement.style.opacity = 1;
 }
