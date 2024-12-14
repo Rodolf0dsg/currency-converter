05a70810ad8d3c1a58eb645c6cca3775579acb74
Rodolfo <fororus12@gmail.com>
2024-12-13 20:46:18 -0400

    Build of select

--- assets/Rates/buildSelect.js
+++ assets/Rates/buildSelect.js
@@ -3,8 +3,7 @@ let countSelect = 0;
 let countMoneda = 0;
 let limitMoneda = 6;
 
-export const buildSelect = (dataCripto) => {
-
+export const buildSelect = (dataCripto, secondOne) => {
     select.addEventListener("click", (e) => {
         if (countSelect < 1) {
 
@@ -12,7 +11,6 @@ export const buildSelect = (dataCripto) => {
                 if (countMoneda >= limitMoneda) {
                     break;
                 }
-
                 const option = document.createElement("option");
                 option.innerText = moneda;
                 select.appendChild(option);
--- assets/index.js
+++ assets/index.js
@@ -3,11 +3,19 @@ import { validateForm } from './form/validateForm.js';
 import { convertRates } from './Rates/convertRates.js';
 import { getRates } from './Rates/getRates.js';
 import { inputListener } from './form/inputListener.js';
+import { buildSelect } from './Rates/buildSelect.js';
 
 const selects = document.getElementsByTagName('select');
 const input = document.getElementsByTagName('input')[0];
 const button = document.getElementsByTagName('button')[0];
 const inputError = document.getElementsByClassName('invalid-feedback');
+
+
+//Obtener los precios
+getRates();
+
+//Construir el input
+buildSelect(JSON.parse(localStorage.getItem('rates')));
 
 //Input no acepte letras, solo numeros
 inputListener(input);
@@ -17,6 +25,7 @@ button.addEventListener('click', (e) => {
     validateForm(input, inputError[1]);
 });
 
+// buildSelect()
 
 // FUNCION QUE RECIBE PARAMETROS PARA MOSTRAR MOENDA
 showResultCurrency("Bitcoin", "10", "Btc a usd");
